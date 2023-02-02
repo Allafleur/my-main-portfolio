84d3074fd789264a3a09313258e97a46d84762b1
Allafleur <104568171+Allafleur@users.noreply.github.com>
2023-02-02 16:20:56 +0100

    Added photos and footer

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("myfooter").innerHTML =
+    `<div class="container">
+    <footer>
+    <div class="justify-content-between contact-box d-none d-md-flex mb-5">
+        <div>
+            <h3 class="mt-4">Work Inquiry</h3>
+            <p>Let's work  together</p>
+        </div>
+        <div>
+            <a href="contact.html" class="btn btn-main mt-4" title="Contact Alla Sydorenko">Contact me</a>
+        </div>
+    </div>
+    <div class="social-links d-flex justify-content-center mb-4">
+        <a href="mailto:allasporty@gmail.com" class="email-link" title="Email Alla"><i class="fa-solid fa-envelope"></i></a>
+        <a href="http://github.com/Allafleur" target="_blank" title="GitHub Profile"><i class="fa-brands fa-github"></i></a>
+        <a href="https://www.facebook.com/alla.sydorenko12" target="_blank" title="Facebook Profile"><i class="fa-brands fa-facebook"></i></a>
+    </div>
+    <p class="text-center mt-5">
+        This project was coded by Alla Sydorenko and it is open-sourced
+    </p>
+    </footer>
+</div>`;
+
+document.querySelector("header").innerHTML += 
+`
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+`;
